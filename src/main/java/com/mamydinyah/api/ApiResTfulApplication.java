48ebe078b7634a788633ce862eeef4181576e6d7
MamyDinyah <mamydinyah34@gmail.com>
2025-03-12 17:04:54 +0300

    initial

---
+++ src/main/java/com/mamydinyah/api/ApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.mamydinyah.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamydinyah/api/ApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamydinyah.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
